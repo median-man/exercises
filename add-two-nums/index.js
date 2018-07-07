5de97d2a72de23ea8cfcc717e0e66b90236006d3
John Desrosiers <johndrunner@yahoo.com>
2018-07-07 10:28:35 -0700

    Remove createNode and createLinkedList methods
    * Remove the factory methods for Node and LinkedList.
    * Add ListNode constructor function from the prompt on leetCode.

--- add-two-nums/index.js
+++ add-two-nums/index.js
@@ -1,22 +1,10 @@
-function createNode(value, next) {
-  return {value, next};
-}
-
-function createLinkedList(...values) {
-  const nodes = [];
-  const addNode = (val, i) => {
-    const node = createNode(val, nodes[i - 1] || null);
-    nodes.push(node);
-  };
-  values.reverse().forEach(addNode);
-
-  const head = nodes[nodes.length - 1];
-
-  return {head};
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
 }
 
 function addTwo() {
 
 }
 
-module.exports = {createNode, createLinkedList, addTwo};
+module.exports = {addTwo};
--- add-two-nums/test.js
+++ add-two-nums/test.js
@@ -1,39 +1,6 @@
 const {expect} = require('chai');
 const {createNode, createLinkedList, addTwo} = require('./index.js');
 
-describe('Node', function() {
-  it('createNode() should be defined', function() {
-    expect(createNode).to.be.a('function');
-  });
-
-  it('createNode() returns a new node object', function() {
-    const actual = createNode(1, {value: 2, next: null});
-    const expected = {
-      value: 1,
-      next: {value: 2, next: null},
-    };
-    expect(actual).to.deep.equal(expected);
-  });
-});
-
-describe('LinkedList', function() {
-  it('createLinkedList should be defined', function() {
-    expect(createLinkedList).to.be.a('function');
-  });
-
-  it('head property should be the first node', function() {
-    const expected = {value: 1, next: null};
-    const actual = createLinkedList(1).head;
-    expect(actual).to.deep.equal(expected);
-  });
-
-  it('next propery of the last node should be null', function() {
-    const expected = {value: 1, next: null};
-    const actual = createLinkedList(1, 1).head.next;
-    expect(actual).to.deep.equal(expected);
-  });
-});
-
 describe('addTwo()', function() {
   it('should be defined', function() {
     expect(addTwo).to.be.a('function');
