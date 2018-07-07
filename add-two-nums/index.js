a50e0a6dfe7ba4e445b095e080948a1a8a6e85be
John Desrosiers <johndrunner@yahoo.com>
2018-07-07 12:38:38 -0700

    addTwo() returns a linked list for the sum of two linked lists
    addTwo accepts the first node of two linked lists for numbers where
    each node is a digit for a base 10 number. The first node for the
    sum of the two numbers is returned.
    
    This is the first solution developed. This solution does not
    edit the constructor for the ListNode in any way and does not store a
    value greater than 9 as the value for a node at any point during
    runtime.

--- add-two-nums/index.js
+++ add-two-nums/index.js
@@ -3,8 +3,28 @@ function ListNode(val) {
   this.next = null;
 }
 
-function addTwo() {
-
+function addTwo(l1, l2) {
+  const hasNext = (n) => n && n.next;
+  let n1 = l1;
+  let n2 = l2;
+  let node = new ListNode(0);
+  const head = node;
+  while (n1 || n2) {
+    let sum = node.val;
+    sum += n1 ? n1.val : 0;
+    sum += n2 ? n2.val : 0;
+    let isOneDigit = sum < 10;
+    let val = isOneDigit ? sum : sum % 10;
+    node.val = val;
+    let nextVal = Math.floor(sum / 10);
+    node.next = (nextVal
+      || hasNext(n1)
+      || hasNext(n2)) ? new ListNode(nextVal) : null;
+    node = node.next;
+    n1 = n1 ? n1.next : null;
+    n2 = n2 ? n2.next : null;
+  }
+  return head;
 }
 
-module.exports = {addTwo};
+module.exports = {addTwo, ListNode};
--- add-two-nums/test.js
+++ add-two-nums/test.js
@@ -1,8 +1,68 @@
 const {expect} = require('chai');
-const {createNode, createLinkedList, addTwo} = require('./index.js');
+const {addTwo, ListNode} = require('./index.js');
+
+const createList = (...vals) => {
+  return vals.reverse().reduce((next, val, index, nodes) => {
+    const node = new ListNode(val);
+    node.next = next;
+    return node;
+  }, null);
+};
+
+describe('ListNode', function() {
+  it('should set val property', function() {
+    const node = new ListNode(1);
+    expect(node.val).to.equal(1);
+  });
+
+  it('next should initialy be null', function() {
+    const node = new ListNode(1);
+    expect(node.next).to.be.null;
+  });
+});
 
 describe('addTwo()', function() {
   it('should be defined', function() {
     expect(addTwo).to.be.a('function');
+  });
+
+  it('should return ListNode where val is 2 when input is 1 and 1', function() {
+    const l1 = createList(1);
+    const l2 = createList(1);
+    const expected = createList(2);
+    const actual = addTwo(l1, l2);
+    expect(actual).to.deep.equal(expected);
+  });
+
+  it('should return first ListNode for 0 -> 1', function() {
+    const l1 = createList(9);
+    const l2 = createList(1);
+    const expected = createList(0, 1);
+    const actual = addTwo(l1, l2);
+    expect(actual).to.deep.equal(expected);
+  });
+
+  it('should return first ListNode for 8 -> 1', function() {
+    const l1 = createList(9);
+    const l2 = createList(9);
+    const expected = createList(8, 1);
+    const actual = addTwo(l1, l2);
+    expect(actual).to.deep.equal(expected);
+  });
+
+  it('should return first ListNode for 1 -> 2', function() {
+    const l1 = createList(9);
+    const l2 = createList(2, 1);
+    const expected = createList(1, 2);
+    const actual = addTwo(l1, l2);
+    expect(actual).to.deep.equal(expected);
+  });
+
+  it('should return first ListNode for 7 -> 0 -> 8', function() {
+    const l1 = createList(2, 4, 3);
+    const l2 = createList(5, 6, 4);
+    const expected = createList(7, 0, 8);
+    const actual = addTwo(l1, l2);
+    expect(actual).to.deep.equal(expected);
   });
 });
