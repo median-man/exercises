a1e9f8d85fa88f9b401ee6e5f35d7bdcafa3902e
John Desrosiers <johndrunner@yahoo.com>
2018-07-07 12:57:45 -0700

    Refactor addTwo()
    Refactor the addTwo method to match the suggested solution from
    leetCode. It is quite similar to my own solution yet somewhate improved
    with reagard to readability.

--- add-two-nums/index.js
+++ add-two-nums/index.js
@@ -4,27 +4,24 @@ function ListNode(val) {
 }
 
 function addTwo(l1, l2) {
-  const hasNext = (n) => n && n.next;
-  let n1 = l1;
-  let n2 = l2;
-  let node = new ListNode(0);
-  const head = node;
-  while (n1 || n2) {
-    let sum = node.val;
-    sum += n1 ? n1.val : 0;
-    sum += n2 ? n2.val : 0;
-    let isOneDigit = sum < 10;
-    let val = isOneDigit ? sum : sum % 10;
-    node.val = val;
-    let nextVal = Math.floor(sum / 10);
-    node.next = (nextVal
-      || hasNext(n1)
-      || hasNext(n2)) ? new ListNode(nextVal) : null;
+  const head = new ListNode(0);
+  let node = head;
+  let carry = 0;
+  let list1 = l1;
+  let list2 = l2;
+  while (list1 || list2) {
+    const val1 = list1 ? list1.val : 0;
+    const val2 = list2 ? list2.val : 0;
+    const sum = val1 + val2 + carry;
+    const val = sum % 10;
+    carry = Math.floor(sum / 10);
+    node.next = new ListNode(val);
     node = node.next;
-    n1 = n1 ? n1.next : null;
-    n2 = n2 ? n2.next : null;
+    list1 = list1 ? list1.next : null;
+    list2 = list2 ? list2.next : null;
   }
-  return head;
+  node.next = carry ? new ListNode(carry) : null;
+  return head.next;
 }
 
 module.exports = {addTwo, ListNode};
