783db73880303ac6194cc775732c7a3ac1ded01a
John Desrosiers <johndrunner@yahoo.com>
2018-07-12 11:50:49 -0700

    Solve median-sorted-arrays
    findMedianSortedArrays() passes all tests and accepted on leetCode.
    However, it does not satisfy the time complexity requirement of
    O(log(min(m,n))). Currently, the solution is O(n/2).

--- median-sorted-arrays/index.js
+++ median-sorted-arrays/index.js
@@ -1,5 +1,32 @@
 function findMedianSortedArrays(nums1, nums2) {
+  const countOfNums = nums1.length + nums2.length; // 6
+  const medianIndex = countOfNums / 2; // 3
+  const allNums = []; // 0, 1, 1, 2
+  let pointer1 = 0; // 2
+  let pointer2 = 0; // 2
 
+  const isUndefined = (val) => typeof val === 'undefined';
+
+  const findNextNum = () => {
+    if (isUndefined(nums2[pointer2]) || nums1[pointer1] < nums2[pointer2]) {
+      allNums.push(nums1[pointer1]);
+      pointer1 += 1;
+    } else {
+      allNums.push(nums2[pointer2]);
+      pointer2 += 1;
+    }
+  };
+
+  while (pointer1 + pointer2 < medianIndex) {
+    findNextNum();
+  }
+
+  if (countOfNums % 2 === 0) {
+    findNextNum();
+    return (allNums[allNums.length - 1] + allNums[allNums.length - 2]) / 2;
+  }
+
+  return allNums[allNums.length - 1];
 }
 
 module.exports = {findMedianSortedArrays};
--- median-sorted-arrays/test.js
+++ median-sorted-arrays/test.js
@@ -5,4 +5,49 @@ describe('findMedianSortedArrays', function() {
   it('should be a function', function() {
     expect(findMedianSortedArrays).to.be.a('function');
   });
+
+  it('should return 1 when given [0, 1] and [2]', function() {
+    const actual = findMedianSortedArrays([0, 1], [2]);
+    expect(actual).to.equal(1);
+  });
+
+  it('should return 2 when given [1, 2] and [3]', function() {
+    const actual = findMedianSortedArrays([1, 2], [3]);
+    expect(actual).to.equal(2);
+  });
+
+  it('should return 2 when given [1] and [2, 3]', function() {
+    const actual = findMedianSortedArrays([1], [2, 3]);
+    expect(actual).to.equal(2);
+  });
+
+  it('should return 2 when given [1, 3] and [2]', function() {
+    const actual = findMedianSortedArrays([1, 3], [2]);
+    expect(actual).to.equal(2);
+  });
+
+  it('should return 2 when given [1] and [3]', function() {
+    const actual = findMedianSortedArrays([1], [3]);
+    expect(actual).to.equal(2);
+  });
+
+  it('should return 2.5 when given [1, 2] and [3, 4]', function() {
+    const actual = findMedianSortedArrays([1, 2], [3, 4]);
+    expect(actual).to.equal(2.5);
+  });
+
+  it('should return 2 when given [2] and []', function() {
+    const actual = findMedianSortedArrays([2], []);
+    expect(actual).to.equal(2);
+  });
+
+  it('should return 2 when given [] and [2]', function() {
+    const actual = findMedianSortedArrays([], [2]);
+    expect(actual).to.equal(2);
+  });
+
+  it('should return 1.5 when given [1, 2, 3, 4] and [0, 1]', function() {
+    const actual = findMedianSortedArrays([1, 2, 3, 4], [0, 1]);
+    expect(actual).to.equal(1.5);
+  });
 });
