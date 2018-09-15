4fe4240d0aa2a971d0cf5d5e80d826df2cfa9b7d
John Desrosiers <johndrunner@yahoo.com>
2018-09-15 06:06:05 -0700

    reverse-integer: first solution
    This is my first solution for reverse(). The solution leverages Array.
    prototype.reverse by first casting the input to a string and then into
    an array before reversing the order and then converting back to a number
    by reversing the process.

--- reverse-integer/index.js
+++ reverse-integer/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse() {
+const reverseDigits = (n) => parseFloat(
+  n.toString().split('').reverse().join('')
+);
 
+module.exports = function reverse(n) {
+  const isNegative = n < 0;
+  return isNegative ? -1 * reverseDigits(-1 * n) : reverseDigits(n);
 };
--- reverse-integer/test.js
+++ reverse-integer/test.js
@@ -5,4 +5,24 @@ describe('reverse()', () => {
   it('is a function', () => {
     expect(reverse).to.be.a('function');
   });
+
+  it('should return 0', () => {
+    expect(reverse(0)).to.equal(0);
+  });
+
+  it('should return 1', () => {
+    expect(reverse(1)).to.equal(1);
+  });
+
+  it('should return 12', () => {
+    expect(reverse(21)).to.equal(12);
+  });
+
+  it('should return -1', () => {
+    expect(reverse(-1)).to.equal(-1);
+  });
+
+  it('should return 21 when the input is 120', () => {
+    expect(reverse(120)).to.equal(21);
+  });
 });
