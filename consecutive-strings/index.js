b69f498bf0f9cc2c580fb1738e1649e837dcda6b
John Desrosiers <john.desro@gmail.com>
2019-04-27 03:50:44 -0700

    consecutive-strings refactoring

--- consecutive-strings/index.js
+++ consecutive-strings/index.js
@@ -1,13 +1,11 @@
-exports.longestSequence = (strings, n) => {
+exports.longestSequence = (strings, n) => { 
   const lastSequenceIndex = strings.length - n;
-  let i = 0;
   let result = '';
-  while (i <= lastSequenceIndex) {
+  for (let i = 0; i <= lastSequenceIndex; i += 1) {
     const current = strings.slice(i, i + n).join('');
     if (current.length > result.length) {
       result = current;
     }
-    i += 1;
   }
   return result;
 };
