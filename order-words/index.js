8d743c4cdaf37b9aecfe74625b762146785890b9
John Desrosiers <john.desro@gmail.com>
2019-05-03 05:50:36 -0700

    Refactor order words

--- order-words/index.js
+++ order-words/index.js
@@ -3,28 +3,12 @@
   https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/javascript
 */
 module.exports.order = (words) => {
-  const wordMap = mapWords(words);
-  const result = [];
-
-  // omits words if word numbers are not consecutive from 1
-  for (let i = 1; i <= wordMap.size; i += 1) {
-    result.push(wordMap[i]);
-  }
-  return result.join(' ');
+  return words
+    .split(' ')
+    .sort(compareWords)
+    .join(' ');
 };
 
-function mapWords(words) {
-  const wordList = words.split(' ');
-  let wordMap = {
-    size: wordList.length,
-  };
-  wordList.forEach((word) => {
-    const num = findNumInWord(word);
-    wordMap[num] = word;
-  });
-  return wordMap;
-}
-
-function findNumInWord(word) {
-  return parseInt(word.replace(/[^1-9]+/g, ''));
+function compareWords(a, b) {
+  return a.match(/\d/) - b.match(/\d/);
 }
