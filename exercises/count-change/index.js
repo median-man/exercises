512cfa1916ebeacac7a374cd9614b2bfdd81b80e
John Desrosiers <john.desro@gmail.com>
2019-05-21 04:46:11 -0700

    countChange refactoring to recursive implementation

--- exercises/count-change/index.js
+++ exercises/count-change/index.js
@@ -1,7 +1,9 @@
-function countChange(amount, coins) {
-  let result = 0;
-  for (let i = 0; i < coins.length; i += 1) {
-    result += amount % coins[i] === 0 ? 1 : 0;
+function countChange(amount, coins, count = 0) {
+  if (amount < 0) return 0;
+  if (amount === 0) return count + 1;
+  let result = count;
+  for (let i = coins.length - 1; i > -1; i -= 1) {
+    result += countChange(amount - coins[i], coins.slice(0, i + 1));
   }
   return result;
 }
