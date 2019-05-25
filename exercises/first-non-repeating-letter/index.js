a370b7ab6044e40f1e5ad97ef9a5a1156d463825
John Desrosiers <john.desro@gmail.com>
2019-05-25 14:27:52 -0700

    Redo first none repeating letter exercise

--- exercises/first-non-repeating-letter/index.js
+++ exercises/first-non-repeating-letter/index.js
@@ -4,63 +4,15 @@
 
 */
 function firstNonRepeatingLetter(s) {
-  const counts = createCharCountMap(s);
-  const doesNotRepeat = (char) => counts.get(char.toLowerCase()) === 1;
-
-  for (let i = 0; i < s.length; i += 1) {
-    if (doesNotRepeat(s[i])) {
-      return s[i];
-    }
+  const counts = {};
+  let letter;
+  for (letter of s.toLowerCase()) {
+    counts[letter] = counts[letter] + 1 || 1;
+  }
+  for (letter of s) {
+    if (counts[letter.toLowerCase()] === 1) return letter;
   }
   return '';
 }
 
-function createCharCountMap(s) {
-  const counts = new Map();
-  for (let i = 0; i < s.length; i += 1) {
-    let lowerCase = s[i].toLowerCase();
-    if (!counts.has(lowerCase)) {
-      counts.set(lowerCase, 0);
-    }
-    counts.set(lowerCase, counts.get(lowerCase) + 1);
-  }
-  return counts;
-}
-
 module.exports = {firstNonRepeatingLetter};
-
-/*
-  PSEUDO-CODE
-
-  input:
-  string s
-  output first character that is not repeated in s
-
-  example:
-  f('stress') = 't'
-
-  should be case insensitive
-  f('sTreSS') = 'T'
-
-  let i = 0
-  let max = length of s
-  let counts = new Map
-  let result = ''
-  WHILE i < max
-    let lowerCase = lower case char
-    IF lowerCase not in counts
-      counts[lowerCase] = 0
-    END IF
-    counts[lowerCase] += 1
-  END WHILE
-
-  let result = ''
-  FOR EACH char in s
-    let lowerCase = lower case char
-    IF counts[lowerCase] === 1 && no result
-      result = char
-    END IF
-  END FOR
-
-  RETURN result
-*/
--- exercises/first-non-repeating-letter/test.js
+++ exercises/first-non-repeating-letter/test.js
@@ -2,31 +2,21 @@ require('chai').should();
 const {firstNonRepeatingLetter} = require('./');
 
 describe('firstNonRepeatingLetter', () => {
-  describe('degenerate tests', () => {
-    it('given an empty string', () => {
-      firstNonRepeatingLetter('').should.equal('');
-    });
+  it('should handle a single letter', () =>
+    firstNonRepeatingLetter('a').should.equal('a'));
 
-    it('given a letter', () => {
-      firstNonRepeatingLetter('a').should.equal('a');
-    });
-  });
+  it('should handle two unique letters', () =>
+    firstNonRepeatingLetter('ab').should.equal('a'));
 
-  describe('basic tests', () => {
-    it('should return "a" given "ab"', () => {
-      firstNonRepeatingLetter('ab').should.equal('a');
-    });
+  it('should return "" given "aa"', () =>
+    firstNonRepeatingLetter('aa').should.equal(''));
 
-    it('should return "" given "aa"', () => {
-      firstNonRepeatingLetter('aa').should.equal('');
-    });
+  it('should return "b" given "abac"', () =>
+    firstNonRepeatingLetter('abac').should.equal('b'));
 
-    it('should return "b" given "aab"', () => {
-      firstNonRepeatingLetter('aab').should.equal('b');
-    });
+  it('should return "b" given "abAc"', () =>
+    firstNonRepeatingLetter('abAc').should.equal('b'));
 
-    it('should return "T" given "sTreSS"', () => {
-      firstNonRepeatingLetter('sTreSS').should.equal('T');
-    });
-  });
+  it('should return "B" given "aBAc"', () =>
+    firstNonRepeatingLetter('aBAc').should.equal('B'));
 });
